e2b9ed22b289970bc17cf678d126a1ab52d6ba71
Alexis <alexisvedrenne482@gmail.com>
2022-03-15 15:59:16 +0100

    Création du base layout et du début de la page index

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,84 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+    <q-header reveal elevated>
+      <q-toolbar inset>
+        <q-toolbar-title><strong>Alexis Vedrenne</strong> Portfolio</q-toolbar-title>
+        <p>Etudiant en cyle ingéniérie | Chef de projet SI</p>
+      </q-toolbar>
+      <q-toolbar class="row justify-center secon">
+        <q-list class="secondary row justify-center">
+          <q-item
+            v-for="(link, index) in linksList"
+            :key="index"
+            :to="link.link"
+            :icon="link.icon"
+            :name="link.title"
+            :label="link.title"
+            class="hn-link"
+            :active="active"
+            active-class="secondary"
+          >
+            <q-item-section avatar class="items-center">
+              <q-icon :name="link.icon" />
+              <p>{{ link.title }}</p>
+            </q-item-section>
+          </q-item>
+        </q-list>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer class="bg-secondary" reveal elevated>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
+  name: "MainLayout",
+  data: function () {
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      linksList: [
+        {
+          title: "Qui suis-je ?",
+          icon: "home",
+          link: "/",
+        },
+        {
+          title: "Passion",
+          caption: "@QuasarFramework",
+          icon: "favorite",
+          link: "hobies",
+        },
+        {
+          title: "Mon parcours scolaire",
+          icon: "hiking",
+          link: "school",
+        },
+        {
+          title: "Mes compétences",
+          icon: "workspace_premium",
+          link: "skills",
+        },
+        {
+          title: "Mes projets",
+          icon: "folder",
+          link: "project",
+        },
+        {
+          title: "Expériences professionnelles",
+          icon: "stacked_line_chart",
+          link: "experience-professional",
+        },
+      ],
+    };
+  },
+});
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,55 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-parallax>
+    <template v-slot:media>
+      <img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </template>
+
+    <template v-slot:content="scope">
+      <div
+        class="absolute column items-center"
+        :style="{
+          opacity: 0.7 + (1 - scope.percentScrolled) * 0.55,
+          top: scope.percentScrolled * 60 + '%',
+          left: 0,
+          right: 0,
+        }"
+      >
+        <img
+          src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+          style="width: 150px; height: 150px"
+        />
+        <div class="text-h3 text-white text-center">Bienvenue sur mon PortFolio</div>
+        <div class="text-h6 text-grey-3 text-center">Dévéloppeur, chef de projet SI</div>
+      </div>
+    </template>
+  </q-parallax>
+  <div class="container">
+    <div class="q-pa-md row justify-center items-center q-gutter-md">
+      <q-card class="my-card">
+        <q-card-section>
+          <p class="text-h2">Information sur le portfolio en lui même</p>
+        </q-card-section>
+        <q-separator inset />
+
+        <q-card-section>
+          <p class="text-h4">L'objectif</p>
+          <p style="width: 1000px">
+            L'objectif de mon portfolio est de monter et d'expliquer mon parcours scolaire
+            et professionel tout au long de ma vie. Dessus on peut y retrouver également
+            tous les projets que j'aurais réaliser tout au long de ma scolairité mais
+            aussi de ma carrière professionelle. Il me permetra d'illustrer de façon
+            simple et précise mes compténces et passions.
+          </p>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
