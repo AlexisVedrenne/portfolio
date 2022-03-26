18abae5628f02a47dac05ed97847fd9cf7157de3
Alexis <alexisvedrenne482@gmail.com>
2022-03-26 18:03:56 +0100

    CV finis

--- src/pages/MyCv.vue
+++ src/pages/MyCv.vue
@@ -1,112 +1,125 @@
 <template>
   <div style="margin-right: 400px; margin-left: 400px">
-    <h6 class="text-center q-mt-lg q-mb-lg">Visualisation de mon CV</h6>
+    <q-intersection transition="scale">
+      <h6 class="text-center q-mt-lg q-mb-lg">Visualisation de mon CV</h6>
+    </q-intersection>
     <div class="row justify-center">
-      <q-btn
-        class="q-mb-md"
-        flat
-        icon="download"
-        color="accent"
-        :loading="loading"
-        @click="toPdf"
-        label="Télécharger le CV"
-      />
+      <q-intersection transition="scale">
+        <q-btn
+          class="q-mb-md"
+          flat
+          icon="download"
+          color="accent"
+          :loading="loading"
+          @click="toPdf"
+          label="Télécharger le CV"
+        />
+      </q-intersection>
     </div>
-    <q-card class="q-mb-lg" id="pdf">
-      <div class="bg-grey column justify-end q-mr-md q-ml-md" style="height: 80px">
-        <h6 class="text-right text-bold q-mt-sm q-mr-sm col text-uppercase">
-          Poste recherché : Une alternance
-        </h6>
-      </div>
-      <q-card-section class="row">
-        <div class="col q-ml-lg">
-          <h3 class="text-bold q-ma-none q-mt-lg">Alexis Vedrenne</h3>
-          <q-separator class="bg-dark" style="height: 8px" spaced />
-          <h5 class="text-bold q-ma-none q-mb-sm q-mt-md">FORMATIONS</h5>
-          <q-list class="bg-grey-2" v-for="(formation, index) in formations" :key="index">
-            <q-item>
-              <q-item-section style="font-size: 17px">
-                <q-item-label>{{ formation.ligne }}</q-item-label>
-                <q-item-label lines="2"
-                  ><strong>{{ formation.lieux }} - </strong
-                  >{{ formation.ligne2 }}</q-item-label
-                >
-              </q-item-section>
-            </q-item>
-
-            <q-separator spaced inset />
-          </q-list>
-          <q-separator class="bg-dark" style="height: 8px" spaced />
-          <h5 class="text-bold q-ma-none q-mt-md q-mb-md">PARCOURS PROFESSIONNEL</h5>
-          <q-list class="bg-grey-2" v-for="(parcour, index) in parcours" :key="index">
-            <q-item>
-              <q-item-section style="font-size: 17px">
-                <q-item-label
-                  ><strong>{{ parcour.poste }},</strong> {{ parcour.ligne }}</q-item-label
-                >
-                <q-item-label lines="2"
-                  ><strong>{{ parcour.lieux }} - </strong
-                  >{{ parcour.ligne2 }}</q-item-label
-                >
-                <p
-                  class="q-ml-lg q-mb-none"
-                  v-for="(tache, index) in parcour.taches"
-                  :key="index"
-                >
-                  {{ index + 1 }} : {{ tache }}
-                </p>
-              </q-item-section>
-            </q-item>
-
-            <q-separator spaced inset />
-          </q-list>
+    <q-intersection transition="scale">
+      <q-card class="q-mb-l" id="pdf">
+        <div class="bg-grey column justify-end q-mr-md q-ml-md" style="height: 80px">
+          <h6 class="text-right text-bold q-mt-sm q-mr-sm col text-uppercase">
+            Poste recherché : Une alternance
+          </h6>
         </div>
-        <div class="col-5 q-ml-lg">
-          <div class="column items-center">
-            <q-img
-              class="col"
-              style="height: 250px; width: 250px"
-              src="~assets/moi.jpg"
-            />
-            <h5 class="text-bold q-ma-none q-mt-md q-mb-sm">COORDONNÉES</h5>
-            <div class="column">
-              <q-btn
-                align="left"
-                v-for="(coordonne, index) in coordonnes"
-                :key="index"
-                :icon="coordonne.icon"
-                :label="coordonne.label"
-                flat
+        <q-card-section class="row">
+          <div class="col q-ml-lg">
+            <h3 class="text-bold q-ma-none q-mt-lg">Alexis Vedrenne</h3>
+            <q-separator class="bg-dark" style="height: 5px" spaced />
+            <h5 class="text-bold q-ma-none q-mb-sm q-mt-md">FORMATIONS</h5>
+            <q-list
+              class="bg-grey-2"
+              v-for="(formation, index) in formations"
+              :key="index"
+            >
+              <q-item :to="{ name: 'aboutMe' }">
+                <q-item-section style="font-size: 17px">
+                  <q-item-label>{{ formation.ligne }}</q-item-label>
+                  <q-item-label lines="2"
+                    ><strong>{{ formation.lieux }} - </strong
+                    >{{ formation.ligne2 }}</q-item-label
+                  >
+                </q-item-section>
+              </q-item>
+
+              <q-separator spaced inset />
+            </q-list>
+            <q-separator class="bg-dark" style="height: 5px" spaced />
+            <h5 class="text-bold q-ma-none q-mt-md q-mb-md">PARCOURS PROFESSIONNEL</h5>
+            <q-list class="bg-grey-2" v-for="(parcour, index) in parcours" :key="index">
+              <q-item :to="{ name: 'experience' }">
+                <q-item-section style="font-size: 17px">
+                  <q-item-label
+                    ><strong>{{ parcour.poste }},</strong>
+                    {{ parcour.ligne }}</q-item-label
+                  >
+                  <q-item-label lines="2"
+                    ><strong>{{ parcour.lieux }} - </strong
+                    >{{ parcour.ligne2 }}</q-item-label
+                  >
+                  <p
+                    class="q-ml-lg q-mb-none"
+                    v-for="(tache, index) in parcour.taches"
+                    :key="index"
+                  >
+                    {{ index + 1 }} : {{ tache }}
+                  </p>
+                </q-item-section>
+              </q-item>
+
+              <q-separator spaced inset />
+            </q-list>
+          </div>
+          <div class="col-5 q-ml-lg">
+            <div class="column items-center">
+              <q-img
+                class="col"
+                style="height: 250px; width: 250px"
+                src="~assets/moi.jpg"
               />
+              <h5 class="text-bold q-ma-none q-mt-md q-mb-sm">COORDONNÉES</h5>
+              <div class="column">
+                <q-btn
+                  href="https://alexis-vedrenne.fr"
+                  target="_blank"
+                  align="left"
+                  v-for="(coordonne, index) in coordonnes"
+                  :key="index"
+                  :icon="coordonne.icon"
+                  :label="coordonne.label"
+                  flat
+                />
+              </div>
+            </div>
+            <q-separator class="bg-dark" style="height: 5px" spaced />
+            <h5 class="text-bold q-ma-none q-mt-md q-mb-sm">COMPÉTENCES</h5>
+            <div class="row text-center">
+              <div class="col">
+                <p class="text-bold">Programmation</p>
+                <p
+                  :style="'color:' + skill.color"
+                  v-for="(skill, index) in skills"
+                  :key="index"
+                  class="q-ma-none"
+                >
+                  <strong style="font-size: 20px">- </strong>{{ skill.label }}
+                </p>
+                <p :style="'color:#080ba8'" class="q-ma-none">
+                  <strong style="font-size: 20px">- </strong>SQL
+                </p>
+              </div>
+              <div class="col">
+                <p class="text-bold">Humaine</p>
+                <p v-for="(humaine, index) in humaines" :key="index" class="q-ma-none">
+                  <strong style="font-size: 20px">- </strong>{{ humaine.label }}
+                </p>
+              </div>
             </div>
           </div>
-          <q-separator class="bg-dark" style="height: 8px" spaced />
-          <h5 class="text-bold q-ma-none q-mt-md q-mb-sm">COMPÉTENCES</h5>
-          <div class="row text-center">
-            <div class="col">
-              <p class="text-bold">Programmation</p>
-              <p
-                :style="'color:' + skill.color"
-                v-for="(skill, index) in skills"
-                :key="index"
-                class="q-ma-none"
-              >
-                <strong style="font-size: 20px">- </strong>{{ skill.label }}
-              </p>
-              <p :style="'color:#080ba8'" class="q-ma-none">
-                <strong style="font-size: 20px">- </strong>SQL
-              </p>
-            </div>
-            <div class="col">
-              <p class="text-bold">Humaine</p>
-              <p v-for="(humaine, index) in humaines" :key="index" class="q-ma-none">
-                <strong style="font-size: 20px">- </strong>{{ humaine.label }}
-              </p>
-            </div>
-          </div>
-        </div>
-      </q-card-section>
-    </q-card>
+        </q-card-section>
+      </q-card>
+    </q-intersection>
     <div id="pdfFin" />
   </div>
 </template>
@@ -142,13 +155,37 @@ export default {
       ],
       parcours: [
         {
-          poste: "Développer",
+          poste: "Assistant Chef de projet / Développeur",
+          ligne: "08/2021 - 08/2022",
+          lieux: "Diese Télécom",
+          ligne2: "Clamart - Alternance en entreprise",
+          taches: [
+            "Management d'une équipe",
+            "Paramétrage de serveur",
+            "Gestion de projet",
+            "Amélioration de programme existant",
+          ],
+        },
+        {
+          poste: "Développeur",
           ligne: "01/2021 - 02/2021",
           lieux: "Vishay",
           ligne2: "Hyere - Stage en entreprise",
           taches: [
             "Programmation d'un robot pour industrialisation",
             "Réalisation de documentation technique",
+          ],
+        },
+        {
+          poste: "Assistant au service infrastructure",
+          ligne: "12/2018 - 01/2019",
+          lieux: "Circert",
+          ligne2: "Sollies-Pont - Stage en entreprise",
+          taches: [
+            "Montage de serveurs",
+            "Configuration de serveurs",
+            "Configuration de switch",
+            "Configuration d'un NAS",
           ],
         },
       ],
