ffe19f5863152192222ef163ef29c9b128b6fdd2
Alexis <alexisvedrenne482@gmail.com>
2022-03-26 19:05:41 +0100

    transtion pour page contact

--- src/pages/ContactMe.vue
+++ src/pages/ContactMe.vue
@@ -1,66 +1,68 @@
 <template>
   <div>
-    <h5 class="text-bold text-center">Vous voulez me contacter ?</h5>
-    <q-form @submit="submit">
-      <div class="row justify-center">
-        <div class="col-5 q-mb-md">
-          <q-input
-            required
-            type="text"
-            label="Entrez votre prénom...*"
-            v-model="contact.prenom"
+    <q-intersection transition="scale">
+      <h5 class="text-bold text-center">Vous voulez me contacter ?</h5>
+      <q-form @submit="submit">
+        <div class="row justify-center">
+          <div class="col-5 q-mb-md">
+            <q-input
+              required
+              type="text"
+              label="Entrez votre prénom...*"
+              v-model="contact.prenom"
+            />
+          </div>
+        </div>
+        <div class="row justify-center">
+          <div class="col-5 q-mb-md">
+            <q-input
+              required
+              type="text"
+              label="Entrez votre nom...*"
+              v-model="contact.nom"
+            />
+          </div>
+        </div>
+        <div class="row justify-center">
+          <div class="col-5 q-mb-md">
+            <q-input
+              type="text"
+              label="Entrez le nom de votre entreprise..."
+              v-model="contact.entreprise"
+            />
+          </div>
+        </div>
+        <div class="row justify-center">
+          <div class="col-5 q-mb-md">
+            <q-input
+              required
+              type="email"
+              label="Entrez votre addresse email...*"
+              v-model="contact.mail"
+            />
+          </div>
+        </div>
+        <div class="row justify-center">
+          <div class="col-5 q-mb-md">
+            <q-input
+              required
+              type="textarea"
+              label="Entre votre message...*"
+              v-model="contact.message"
+            />
+          </div>
+        </div>
+        <div class="row justify-center">
+          <q-btn
+            icon-right="send"
+            color="accent"
+            :loading="loading"
+            label="Envoyer le message"
+            type="submit"
           />
         </div>
-      </div>
-      <div class="row justify-center">
-        <div class="col-5 q-mb-md">
-          <q-input
-            required
-            type="text"
-            label="Entrez votre nom...*"
-            v-model="contact.nom"
-          />
-        </div>
-      </div>
-      <div class="row justify-center">
-        <div class="col-5 q-mb-md">
-          <q-input
-            type="text"
-            label="Entrez le nom de votre entreprise..."
-            v-model="contact.entreprise"
-          />
-        </div>
-      </div>
-      <div class="row justify-center">
-        <div class="col-5 q-mb-md">
-          <q-input
-            required
-            type="email"
-            label="Entrez votre addresse email...*"
-            v-model="contact.mail"
-          />
-        </div>
-      </div>
-      <div class="row justify-center">
-        <div class="col-5 q-mb-md">
-          <q-input
-            required
-            type="textarea"
-            label="Entre votre message...*"
-            v-model="contact.message"
-          />
-        </div>
-      </div>
-      <div class="row justify-center">
-        <q-btn
-          icon-right="send"
-          color="accent"
-          :loading="loading"
-          label="Envoyer le message"
-          type="submit"
-        />
-      </div>
-    </q-form>
+      </q-form>
+    </q-intersection>
   </div>
 </template>
 <script>
