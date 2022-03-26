0e744cb27447c9e9f71259185df9443c6f552dc8
Alexis <alexisvedrenne482@gmail.com>
2022-03-26 19:02:58 +0100

    Mise en forme du formulaire de contact

--- src/pages/ContactMe.vue
+++ src/pages/ContactMe.vue
@@ -1,30 +1,54 @@
 <template>
   <div>
     <h5 class="text-bold text-center">Vous voulez me contacter ?</h5>
-    <q-form>
+    <q-form @submit="submit">
       <div class="row justify-center">
         <div class="col-5 q-mb-md">
-          <q-input required type="text" label="Entrez votre prénom...*" />
+          <q-input
+            required
+            type="text"
+            label="Entrez votre prénom...*"
+            v-model="contact.prenom"
+          />
         </div>
       </div>
       <div class="row justify-center">
         <div class="col-5 q-mb-md">
-          <q-input required type="text" label="Entrez votre nom...*" />
+          <q-input
+            required
+            type="text"
+            label="Entrez votre nom...*"
+            v-model="contact.nom"
+          />
         </div>
       </div>
       <div class="row justify-center">
         <div class="col-5 q-mb-md">
-          <q-input required type="text" label="Entrez le nom de votre entreprise..." />
+          <q-input
+            type="text"
+            label="Entrez le nom de votre entreprise..."
+            v-model="contact.entreprise"
+          />
         </div>
       </div>
       <div class="row justify-center">
         <div class="col-5 q-mb-md">
-          <q-input required type="email" label="Entrez votre addresse email...*" />
+          <q-input
+            required
+            type="email"
+            label="Entrez votre addresse email...*"
+            v-model="contact.mail"
+          />
         </div>
       </div>
       <div class="row justify-center">
         <div class="col-5 q-mb-md">
-          <q-input required type="textarea" label="Entre votre message...*" />
+          <q-input
+            required
+            type="textarea"
+            label="Entre votre message...*"
+            v-model="contact.message"
+          />
         </div>
       </div>
       <div class="row justify-center">
@@ -33,18 +57,37 @@
           color="accent"
           :loading="loading"
           label="Envoyer le message"
+          type="submit"
         />
       </div>
     </q-form>
   </div>
 </template>
 <script>
-import * as mailjet from "node-mailjet";
+import { useQuasar } from "quasar";
 export default {
   data() {
     return {
       loading: false,
+      utils: useQuasar(),
+      contact: {
+        nom: "",
+        prenom: "",
+        entreprise: "",
+        message: "",
+        mail: "",
+      },
     };
+  },
+  methods: {
+    submit() {
+      this.loading = true;
+      this.utils.notify({
+        message: "Le service d'envoie est actuellement indisponible...",
+        color: "negative",
+      });
+      this.loading = false;
+    },
   },
 };
 </script>
